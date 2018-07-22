a96038eb2e2abe8b1ae4e402556e7336d18096c7
derrick b <dbozich1@gmail.com>
2018-07-22 02:50:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,49 +1,11 @@
-# require './environment'
-# require_relative 'app/models/pirate'
-# require_relative 'app/models/ship'
-# 
-# module FormsLab
-#   class App < Sinatra::Base
-# 
-#     # code other routes/actions here
-#     get '/' do
-#       erb :root
-#     end
-# 
-#     get '/new' do
-#       erb :'pirates/new'
-#     end
-# 
-#     post '/pirates' do
-# 
-#       @pirate = Pirate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height])
-#       # @ship1 = Ship.new(pirate[ships][][:name],pirate[ships][][:type],pirate[ships][][:booty])
-#       # @ship2 = Ship.new(pirate[ships][][:name],pirate[ships][][:type],pirate[ships][][:booty])
-# 
-#       params[:pirate][:ships].each do |details|
-#         Ship.new(details)
-#         # puts details
-#       end
-# 
-# 
-#       @ships = Ship.all
-#       # binding.pry
-# 
-# 
-#       erb :'pirates/show'
-#     end
-# 
-#     get '/pirates' do
-#       erb :'pirates/show'
-#     end
-# 
-#   end
-# end
-
 require './environment'
+require_relative 'app/models/pirate'
+require_relative 'app/models/ship'
 
 module FormsLab
   class App < Sinatra::Base
+
+    # code other routes/actions here
     get '/' do
       erb :root
     end
@@ -53,15 +15,27 @@ module FormsLab
     end
 
     post '/pirates' do
-      @pirate = Pirate.new(params[:pirate])
+
+      @pirate = Pirate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height])
+      # @ship1 = Ship.new(pirate[ships][][:name],pirate[ships][][:type],pirate[ships][][:booty])
+      # @ship2 = Ship.new(pirate[ships][][:name],pirate[ships][][:type],pirate[ships][][:booty])
 
       params[:pirate][:ships].each do |details|
         Ship.new(details)
+        # puts details
       end
+
+
       @ships = Ship.all
+      # binding.pry
+
 
       erb :'pirates/show'
     end
-    
+
+    get '/pirates' do
+      erb :'pirates/show'
+    end
+
   end
 end
