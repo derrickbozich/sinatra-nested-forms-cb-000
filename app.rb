c1ecc1a9edb138f62fc686b78aee43f357a85d90
derrick b <dbozich1@gmail.com>
2018-07-22 02:30:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ module FormsLab
       # @ship1 = Ship.new(pirate[ships][][:name],pirate[ships][][:type],pirate[ships][][:booty])
       # @ship2 = Ship.new(pirate[ships][][:name],pirate[ships][][:type],pirate[ships][][:booty])
 
-      pirate[:ships].each do |details|
+      params[:pirate][:ships].each do |details|
         Ship.new(details)
       end
 
