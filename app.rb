e5e558eab2e0c8c8358945778663f9a733cc80f0
derrick b <dbozich1@gmail.com>
2018-07-22 02:24:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,9 +1,33 @@
 require './environment'
+# require_relative './app/models/pirate'
+# require_relative './app/models/ship'
 
 module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(pirate[:name], pirate[:weight], pirate[:height])
+      # @ship1 = Ship.new(pirate[ships][][:name],pirate[ships][][:type],pirate[ships][][:booty])
+      # @ship2 = Ship.new(pirate[ships][][:name],pirate[ships][][:type],pirate[ships][][:booty])
+
+      pirate[:ships].each do |details|
+        Ship.new(details)
+      end
+
+      @ships = Ship.all
+      binding.pry
+
+      erb :'pirates/show'
+    end
 
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  def initialize(params)
+    @name = pirate[:ships][:name]
+    @type = pirate[:ships][:type]
+    @booty = pirate[:ships][:booty]
+    @@all << self
+  end
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
 end
