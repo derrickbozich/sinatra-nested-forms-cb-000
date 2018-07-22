69a683e4523dc09e0c8ddccacb109af0a95ddf81
derrick b <dbozich1@gmail.com>
2018-07-22 02:30:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ module FormsLab
 
     post '/pirates' do
       binding.pry
-      @pirate = Pirate.new(pirate[:name], pirate[:weight], pirate[:height])
+      @pirate = Pirate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height])
       # @ship1 = Ship.new(pirate[ships][][:name],pirate[ships][][:type],pirate[ships][][:booty])
       # @ship2 = Ship.new(pirate[ships][][:name],pirate[ships][][:type],pirate[ships][][:booty])
 
